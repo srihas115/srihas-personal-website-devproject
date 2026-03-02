433d905192d69d7875d44177011185d91609c006
srihas <srihasgupta@gmail.com>
2026-03-02 16:03:50 -0600

    added my pfp as an image

--- app/about/HelloWorld.jsx
+++ app/about/HelloWorld.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
 
 const HelloWorld = () => {
   return (
     <div>
       <h1>Hello, world!</h1>
+      <Image src="/images/pfp.png" alt="Srihas" width={240} height={240} />
       <p>My name is Srihas and this is my first React component.</p>
     </div>
   );
